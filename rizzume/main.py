f50a93b8bda7eee838c6cafaed160cb4894e251f
mariooohzc <mariohanzel@outlook.com>
2024-05-28 10:56:30 +0800

    added feature 7 which works completely well, added groq and docx lib, updated index.html and addded docx_upload4AI.html and added testing.js

--- rizzume/main.py
+++ rizzume/main.py
@@ -1,12 +1,25 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, File, UploadFile
 from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+import os
+import re
+
+
+#AI feeback generator libraries
+from docx import Document
+from io import BytesIO
+from groq import Groq
+
+
+
 
 app = FastAPI()
 
-# Templates directory
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 class ResumeRequest(BaseModel):
     full_name: str
@@ -14,6 +27,8 @@ class ResumeRequest(BaseModel):
     phone: str
     skills: str
 
+
+#Feature 1: Input Data to generate Resume
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
@@ -38,7 +53,40 @@ async def generate_resume(request: Request, full_name: str = Form(...), email: s
     """
     return HTMLResponse(content=resume_template)
 
-@app.get("/fill-up", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("fill_up_form.html", {"request": request})
+
+
+#Feature 7: Upload of resume for AI feedback
+
+@app.get("/docx-submit", response_class=HTMLResponse)
+def submit_docx(request: Request):
+    return templates.TemplateResponse("docx_upload4AI.html", {"request": request})
+
+
+@app.post("/feedback", response_class = HTMLResponse)
+async def feedback(request: Request, feedback_file: UploadFile = File(...)):
+    doc = Document(BytesIO(await feedback_file.read()))
+    text_lines = [para.text for para in doc.paragraphs]
+    full_text = "\n".join(text_lines)
+    
+    client = Groq(api_key= os.environ.get("GROQ_API_KEY"))
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": f"Is the resume good? if it is good, please say so. Else, give advice on how to improve the resume: {full_text}",
+            }
+        ],
+        model="llama3-8b-8192",
+    )
+
+    result = chat_completion.choices[0].message.content
+    result_l = result.split("\n")
+
+    return templates.TemplateResponse("docx_upload4AI.html", {"request":request, "result_l": result_l})
+
+
+
+
+
 
