6ef690af08fef61956773a1d628ba2d6632a6fff
mariooohzc <mariohanzel@outlook.com>
2024-07-14 14:23:37 +0800

    added do's and don'ts for ai feedback feature

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from groq import Groq
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 # web scraping feature part 1
